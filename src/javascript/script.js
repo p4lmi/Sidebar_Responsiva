a0ad01e8c24dada4ccd948be29e8211a87a11d57
Matheus Fernandes de Sousa <matheus.fernan97@gmail.com>
2025-07-09 21:18:45 -0300

    Add files via upload

---
+++ src/javascript/script.js
@@ -0,0 +1,3 @@
+document.getElementById('open_btn').addEventListener('click', function () {
+    document.getElementById('sidebar').classList.toggle('open-sidebar');
+}); // controla botão da sidebar, abrindo e fechando o menu de opções
